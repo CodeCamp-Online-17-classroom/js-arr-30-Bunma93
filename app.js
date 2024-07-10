16c4235d3015aa5bcf86ce61de3623901edb22d1
Bunma93 <bunma93@gmail.com>
2024-07-10 15:19:46 +0700

    Js-arr30

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+const array1 = ['apple', 'banana', 'orange'];
+const array2 = [1, 3, 4, 5, 6, 7, 8];
+const array3 = [1, -3, 2, 8, -4, 5];
+
+let result1 = array1.map(x => x.toUpperCase());
+let result2 = array2.map(x => x%2 === 0 ? "even" : "odd");
+let result3 = array3.map(x => Math.abs(x));
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
